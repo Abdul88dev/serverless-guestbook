c9be185a51b35060e7b5de56fa85e2cfba514be1
Abdulmohsen alshalabi <76660823+Abdul88dev@users.noreply.github.com>
2022-08-31 23:45:42 +0200

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://28df21f0.eu-de.apigw.appdomain.cloud/guestbook/entries';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
